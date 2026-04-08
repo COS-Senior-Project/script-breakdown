fc4536c8cb6af1b8499682f1e5080d6db5ea2f9b
viliandoneva <vad220@aubg.edu>
2026-04-08 15:13:37 +0300

    Basic UI schedule table

--- big-break-ui/src/App.jsx
+++ big-break-ui/src/App.jsx
@@ -40,40 +40,72 @@ function App() {
 
             <div style={{marginTop: "30px"}}>
                 {schedule.map((day) => (
-                    <div key={day.dayNumber} style={{ marginBottom: "20px" }}>
+                    <div key={day.dayNumber} style={{ marginBottom: "40px" }}>
                         <h2>Day {day.dayNumber}</h2>
-                        
-                        Page length: {day.pageCount}<br/>
-                        Time: {day.time}<br/>
-                        Move Count: {day.moveCount}<br/>
-                        Locations:<br/>
-                        {day.locationSet.map((location) => (
-                            <div key={location}>{location}</div>
-                        ))}
-                         
 
-                        {day.scenes.map((scene) => (
-                            <div key={scene.sceneNumber}>
-                                <h4>Scene {scene.sceneNumber}</h4>
-                                Scene Heading: {scene.heading}<br/>
-                                INT/EXT: {scene.locationKeyword}<br/>
-                                Location: {scene.location}<br/>
-                                Scene Shooting Time: {scene.shootPhase}<br/>
-                                Scene Length: {scene.pageCount}<br/>
-                                Characters:<br/>
-                                {scene.canonicalCharacterNames.map((character) => (
-                                    <div key={character}>{character}</div>
-                                ))}
-                                Possible Characters:<br/>
-                                {scene.charactersBelowConfidence.map((character) => (
-                                    <div key={character}>{character}</div>
-                                ))}
-                                {/* Characters:<br/>
-                                {scene.charactersWithScores.map((character) => (
-                                    <div key={character}>{character}</div>
-                                ))} */}
-                            </div>
-                        ))}
+                        <div style={{ marginBottom: "20px", fontSize: "16px" }}>
+                            <strong>Page length: </strong>{day.pageCount}<br/>
+                            <strong>Time:</strong> {day.time}<br/>
+                            <strong>Move Count:</strong> {day.moveCount}<br/>
+                            <strong>Locations</strong>: {day.locationSet?.join(", ")}
+                        </div>
+
+                        <table border="1" cellPadding="4" style={{ borderCollapse: "collapse", width: "100%", fontSize: "14px" }}>
+                            <thead>
+                                <tr>
+                                    <th>Scene Number</th>
+                                    <th>Heading</th>
+                                    <th>INT/EXT</th>
+                                    <th>Location</th>
+                                    <th>Scene Shooting Time</th>
+                                    <th>Scene Length</th>
+                                    <th>Characters</th>
+                                    <th>Possible Characters</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {day.scenes.map((scene) => {
+                                    const isINT = scene.locationKeyword === "INT";
+                                    const isEXT = scene.locationKeyword === "EXT";
+                                    const isDAY = scene.shootPhase === "DAY";
+                                    const isNIGHT = scene.shootPhase === "NIGHT";
+
+                                    let backgroundColor = "white";
+                                    let textColor = "black";
+
+                                    if (isINT && isDAY) {
+                                        backgroundColor = "white";
+                                    }
+                                    else if (isEXT && isDAY) {
+                                        backgroundColor = "#fef08a"
+                                    }
+                                    else if (isINT && isNIGHT) {
+                                        backgroundColor = "#bbf7d0"
+                                    }
+                                    else if (isEXT && isNIGHT) {
+                                        backgroundColor = "#1e3a8a"
+                                        textColor = "white";
+                                    }
+                                    return (
+                                        <tr key={`${day.dayNumber}-${scene.sceneNumber}`}
+                                        style={{
+                                            backgroundColor,
+                                            color: textColor
+                                        }}>
+                                            <td>{scene.sceneNumber}</td>
+                                            <td>{scene.heading}</td>
+                                            <td>{scene.locationKeyword}</td>
+                                            <td>{scene.location}</td>
+                                            <td>{scene.shootPhase}</td>
+                                            <td>{scene.pageCount}</td>
+                                            <td>{scene.canonicalCharacterNames?.join(", ")}</td>
+                                            <td>{scene.charactersBelowConfidence?.join(", ")}</td>
+                                            {/* <td>{scene.charactersWithScores?.join(", ")}</td> */}
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
                     </div>
                 ))}
             </div>
